da8bdc6a233879a94c0545f39f7875ed7773886a
dpvnbd <dpvnbd@gmail.com>
2018-01-02 12:36:50 +0200

    Configure the cookie authentication

--- DMS.Web/Startup.cs
+++ DMS.Web/Startup.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DMS.Infrastructure.Data;
+using DMS.Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,10 +17,31 @@ namespace DMS.Web
 {
   public class Startup
   {
+    private IConfiguration Configuration;
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DMS")));
+
+      services.AddIdentity<AppIdentityUser, IdentityRole>(options =>
+      {
+        // Password settings
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 4;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequiredUniqueChars = 1;
+      })
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
       services.AddMvc();
     }
 
@@ -27,12 +53,9 @@ namespace DMS.Web
         app.UseDeveloperExceptionPage();
       }
 
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-            name: "default",
-            template: "{controller=Home}/{action=Index}/{id?}");
-      });
+      app.UseAuthentication();
+
+      app.UseMvcWithDefaultRoute();
     }
   }
 }
