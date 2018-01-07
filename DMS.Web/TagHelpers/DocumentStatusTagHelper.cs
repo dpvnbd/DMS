c5a5268cfc0a0b6be788162a49a0b2837d7cd10a
dpvnbd <dpvnbd@gmail.com>
2018-01-07 22:15:29 +0200

    Add DocumentStatus tag helper to display colored badges with status

---
+++ DMS.Web/TagHelpers/DocumentStatusTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using DMS.Domain.Entities;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Web.TagHelpers
+{
+  public class DocumentStatusTagHelper : TagHelper
+  {
+    public DocumentStatus Status { get; set; }
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+      output.TagName = "span";
+      string badgeColor = "badge-secondary";
+
+      output.Content.SetHtmlContent(Status.ToString());
+
+      switch (Status)
+      {
+        case DocumentStatus.Created:
+          badgeColor = "badge-secondary";
+          break;
+        case DocumentStatus.Submitted:
+          badgeColor = "badge-info";
+          break;
+        case DocumentStatus.Rejected:
+          badgeColor = "badge-warning";
+          break;
+        case DocumentStatus.Approved:
+          badgeColor = "badge-primary";
+          break;
+        case DocumentStatus.Accepted:
+          badgeColor = "badge-success";
+          break;
+        case DocumentStatus.Declined:
+          badgeColor = "badge-danger";
+          break;        
+      }
+
+      output.Attributes.Add("class", "badge " + badgeColor);
+
+      output.TagMode = TagMode.StartTagAndEndTag;
+    }
+  }
+}
