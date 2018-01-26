935f6a6c3490c0c5df6ce45b28bedc719f39603b
dpvnbd <dpvnbd@gmail.com>
2018-01-26 19:23:53 +0200

    Add base class for Value Objects

---
+++ DMS.Domain/Abstract/BaseValueObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMS.Domain.Abstract
+{
+  public abstract class BaseValueObject
+  {
+    protected static bool EqualOperator(BaseValueObject left, BaseValueObject right)
+    {
+      if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+      {
+        return false;
+      }
+      return ReferenceEquals(left, null) || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(BaseValueObject left, BaseValueObject right)
+    {
+      return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetAtomicValues();
+
+    public override bool Equals(object obj)
+    {
+      if (obj == null || obj.GetType() != GetType())
+      {
+        return false;
+      }
+
+      BaseValueObject other = (BaseValueObject)obj;
+      IEnumerator<object> thisValues = GetAtomicValues().GetEnumerator();
+      IEnumerator<object> otherValues = other.GetAtomicValues().GetEnumerator();
+      while (thisValues.MoveNext() && otherValues.MoveNext())
+      {
+        if (ReferenceEquals(thisValues.Current, null) ^
+            ReferenceEquals(otherValues.Current, null))
+        {
+          return false;
+        }
+
+        if (thisValues.Current != null &&
+            !thisValues.Current.Equals(otherValues.Current))
+        {
+          return false;
+        }
+      }
+      return !thisValues.MoveNext() && !otherValues.MoveNext();
+    }
+    // Other utilility methods
+  }
+}
