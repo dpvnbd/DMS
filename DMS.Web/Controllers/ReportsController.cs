cae17b8c7c9c53444532a93b342f10e64ac77d6f
dpvnbd <dpvnbd@gmail.com>
2018-01-29 19:50:00 +0200

    Add date filter for reports

--- DMS.Web/Controllers/ReportsController.cs
+++ DMS.Web/Controllers/ReportsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DMS.Application.Reports;
+using DMS.Web.Models.Reports;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DMS.Web.Controllers
@@ -19,12 +20,28 @@ namespace DMS.Web.Controllers
     public async Task<IActionResult> StatusCount(int userId = -1, DateTime? from = null, DateTime? to = null)
     {
       var model = await reportsService.GetStatusCount(userId, from, to);
+      var filter = new ReportsFilterViewModel
+      {
+        UserId = userId,
+        From = from,
+        To = to
+      };
+
+      ViewData["filter"] = filter;
       return View(model);
     }
 
     public async Task<IActionResult> ReviewTime(int documentId = -1, DateTime? from = null, DateTime? to = null)
     {
       var model = await reportsService.GetStatusChangesTime(documentId, from, to);
+      var filter = new ReportsFilterViewModel
+      {
+        DocumentId = documentId,
+        From = from,
+        To = to
+      };
+
+      ViewData["filter"] = filter;
       return View(model);
     }
   }
---
+++ DMS.Web/Models/Reports/ReportsFilterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMS.Web.Models.Reports
+{
+  public class ReportsFilterViewModel
+  {
+    public int? DocumentId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime? From { get; set; }
+
+    public DateTime? To { get; set; }
+  }
+}
